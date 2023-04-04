b94b40914c464c12426a0fce8ff9b029c204202e
wavvs <30052411+wavvs@users.noreply.github.com>
2023-04-04 14:35:01 +0300

    Add pinger

---
+++ misc/Pinger/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+using NetTools;
+
+namespace Pinger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("[!] Not enough arguments");
+                return;
+            }
+
+            var list = args[0].Split(',');
+            var addresses = new List<IPAddress>();
+            foreach (var item in list)
+            {
+                IPAddressRange range;
+                var result = IPAddressRange.TryParse(args[0], out range);
+                if (!result)
+                {
+                    Console.WriteLine("[!] Cannot parse IP address range.");
+                    return;
+                }
+                foreach (var ip in range)
+                {
+                    addresses.Add(ip);
+                }
+            }
+
+            var tasks = new List<Task<PingReply>>();
+            foreach (var addr in addresses)
+            {
+                tasks.Add(PingAsync(addr));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            foreach (var task in tasks)
+            {
+                if (task.Result.Status == IPStatus.Success)
+                {
+                    Console.WriteLine($"[+] {task.Result.Address}");
+                }
+            }
+        }
+
+        static Task<PingReply> PingAsync(IPAddress address)
+        {
+            var tcs = new TaskCompletionSource<PingReply>();
+            Ping ping = new Ping();
+            
+            ping.PingCompleted += (obj, sender) =>
+            {
+                tcs.SetResult(sender.Reply);
+            };
+            ping.SendAsync(address, 5000, new object());
+            return tcs.Task;
+        }
+    }
+}
--- misc/install.sh
+++ misc/install.sh
@@ -6,6 +6,7 @@ SLIVER_PATH="/opt/sliver"
 HAVOC_PATH="/opt/havoc"
 NGINX_PATH="/opt/nginx"
 ARTIFACTS_PATH="/opt/artifacts"
+WORDLISTS_PATH="/opt/wordlists"
 
 prereq(){
 # Prerequisites
@@ -21,6 +22,8 @@ bh() {
     sudo chown $USER:$USER $BH_PATH
     wget https://github.com/ly4k/BloodHound/releases/latest/download/BloodHound-linux-x64.zip -O $BH_PATH/BloodHound-linux-x64.zip
     unzip "$BH_PATH/BloodHound-linux-x64.zip" -d $BH_PATH
+    # https://github.com/CompassSecurity/BloodHoundQueries
+    curl -o ~/.config/bloodhound/customqueries.json "https://raw.githubusercontent.com/CompassSecurity/BloodHoundQueries/master/BloodHound_Custom_Queries/customqueries.json"
 }
 
 sliver(){
@@ -69,8 +72,10 @@ artifacts() {
     git clone https://github.com/FuzzySecurity/StandIn.git $ARTIFACTS_PATH/wnd/assemblies/StandIn
     # CheeseSQL (remove FodyWeavers)
     git clone https://github.com/klezVirus/CheeseTools.git $ARTIFACTS_PATH/wnd/assemblies/CheeseTools
+    # SharpShares
     wget https://github.com/mitchmoser/SharpShares/releases/latest/download/SharpShares.exe -O $ARTIFACTS_PATH/wnd/assemblies/curated
-    
+    # SharpRPC
+    git clone https://github.com/wavvs/SharpRPC.git $ARTIFACTS_PATH/wnd/assemblies/SharpRPC
     # ---------- C# assemblies ----------
 
     # doctrack
@@ -83,29 +88,38 @@ artifacts() {
     rm -rf $ARTIFACTS_PATH/doctrack
     cd $CURPATH
     # chisel
-    wget https://github.com/jpillora/chisel/releases/latest/download/chisel_1.8.1_linux_amd64.gz -O $ARTIFACTS_PATH/lnx/chisel.gz
+    wget https://github.com/jpillora/chisel/releases/download/v1.8.1/chisel_1.8.1_linux_amd64.gz -O $ARTIFACTS_PATH/lnx/chisel.gz
     cd $ARTIFACTS_PATH/lnx
     gzip -d chisel.gz && chmod +x chisel
-    wget https://github.com/jpillora/chisel/releases/latest/download/chisel_1.8.1_windows_amd64.gz -O $ARTIFACTS_PATH/wnd/chisel.gz
+    wget https://github.com/jpillora/chisel/releases/download/v1.8.1/chisel_1.8.1_windows_amd64.gz -O $ARTIFACTS_PATH/wnd/chisel.gz
     cd $ARTIFACTS_PATH/wnd
     gzip -d chisel.gz && mv chisel chisel.exe
     cd $CURPATH
     # wiretap
-    wget https://github.com/sandialabs/wiretap/releases/latest/download/wiretap_0.2.1_linux_amd64.tar.gz -O $ARTIFACTS_PATH/lnx/wiretap-lnx.tar.gz
-    tar -xf $ARTIFACTS_PATH/lnx/wiretap-lnx.tar.gz -C $ARTIFACTS_PATH
+    wget https://github.com/sandialabs/wiretap/releases/download/v0.2.1/wiretap_0.2.1_linux_amd64.tar.gz -O $ARTIFACTS_PATH/lnx/wiretap-lnx.tar.gz
+    tar -xf $ARTIFACTS_PATH/lnx/wiretap-lnx.tar.gz -C $ARTIFACTS_PATH/lnx
     chmod +x $ARTIFACTS_PATH/lnx/wiretap
     rm $ARTIFACTS_PATH/lnx/wiretap-lnx.tar.gz
-    wget https://github.com/sandialabs/wiretap/releases/latest/download/wiretap_0.2.1_windows_amd64.tar.gz -O $ARTIFACTS_PATH/wnd/wiretap-wnd.tar.gz
-    tar -xf $ARTIFACTS_PATH/wnd/wiretap-wnd.tar.gz -C $ARTIFACTS_PATH
+    wget https://github.com/sandialabs/wiretap/releases/download/v0.2.1/wiretap_0.2.1_windows_amd64.tar.gz -O $ARTIFACTS_PATH/wnd/wiretap-wnd.tar.gz
+    tar -xf $ARTIFACTS_PATH/wnd/wiretap-wnd.tar.gz -C $ARTIFACTS_PATH/lnx
     rm $ARTIFACTS_PATH/wnd/wiretap-wnd.tar.gz
     # mimikatz
     wget https://github.com/gentilkiwi/mimikatz/releases/latest/download/mimikatz_trunk.zip -O $ARTIFACTS_PATH/wnd/mimikatz.zip
     unzip $ARTIFACTS_PATH/wnd/mimikatz.zip -d $ARTIFACTS_PATH/wnd/mimikatz && rm $ARTIFACTS_PATH/wnd/mimikatz.zip
     # ysoserial.net
-    wget https://github.com/pwntester/ysoserial.net/releases/latest/download/ysoserial-1.35.zip -O $ARTIFACTS_PATH/wnd/ysoserial.zip
+    wget https://github.com/pwntester/ysoserial.net/releases/download/v1.35/ysoserial-1.35.zip -O $ARTIFACTS_PATH/wnd/ysoserial.zip
     unzip $ARTIFACTS_PATH/wnd/ysoserial.zip -d $ARTIFACTS_PATH/wnd/ysoserial && rm $ARTIFACTS_PATH/wnd/ysoserial.zip
+    # ffuf 
+    wget https://github.com/ffuf/ffuf/releases/download/v2.0.0/ffuf_2.0.0_linux_amd64.tar.gz -O $ARTIFACTS_PATH/lnx/ffuf.tar.gz
+    tar -xf $ARTIFACTS_PATH/lnx/ffuf.tar.gz -C $ARTIFACTS_PATH/lnx
+    chmod +x $ARTIFACTS_PATH/lnx/ffuf
+    rm $ARTIFACTS_PATH/lnx/ffuf.tar.gz
+    # nmap 
+    wget https://github.com/andrew-d/static-binaries/raw/master/binaries/windows/x86/nmap.exe -O $ARTIFACTS_PATH/wnd/nmap.exe
+    # linpeas
+    wget https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas_linux_amd64 -O $ARTIFACTS_PATH/lnx/linpeas.sh
 
-    # ----------- Other --------------
+    # ----------- AD & Kerberos --------------
     # nanorobeus
     git clone https://github.com/wavvs/nanorobeus.git $ARTIFACTS_PATH/other/nanorobeus
     cd $ARTIFACTS_PATH/other/nanorobeus && make bof
@@ -117,11 +131,27 @@ artifacts() {
     mv nanorobeus.x* $ARTIFACTS_PATH/other/nanorobeus
     mv extension.json $ARTIFACTS_PATH/other/nanorobeus
     cd $CURPATH
+    # minikerberos
     git clone https://github.com/skelsec/minikerberos.git $ARTIFACTS_PATH/other/minikerberos
     cd $ARTIFACTS_PATH/other/minikerberos
     sudo python3 setup.py install
     cd $CURPATH
-    # ----------- Other --------------
+    # KeytabParser
+    git clone https://github.com/its-a-feature/KeytabParser.git $ARTIFACTS_PATH/other/KeytabParser
+    sudo pip3 install ldapdomaindump
+    sudo pip3 install bloodhound
+    sudo pip3 install certipy-ad
+    sudo pip3 install git+https://github.com/dirkjanm/adidnsdump#egg=adidnsdump
+    # AD bridge open
+    wget https://github.com/BeyondTrust/pbis-open/releases/download/9.1.0/pbis-open-9.1.0.551.linux.x86_64.deb.sh -O $ARTIFACTS_PATH/lnx/pbis.sh
+    chmod +x $ARTIFACTS_PATH/lnx/pbis.sh && sudo sh $ARTIFACTS_PATH/lnx/pbis.sh && rm $ARTIFACTS_PATH/lnx/pbis.sh
+    # ----------- AD & Kerberos --------------
+}
+
+wordlists() {
+    sudo mkdir -p $WORDLISTS_PATH
+    sudo chown $USER:$USER $WORDLISTS_PATH
+    git clone https://github.com/danielmiessler/SecLists.git $WORDLISTS_PATH/seclists
 }
 
 all() {
@@ -131,6 +161,7 @@ all() {
     havoc
     nginx
     artifacts
+    wordlists
 }
 
 "$@"
